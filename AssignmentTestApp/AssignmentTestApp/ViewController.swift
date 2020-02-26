37c311db74565db7f6c8b212581421db60cb2f20
Akash <akash.revanna@gmail.com>
2020-02-26 18:31:14 +0530

    - Created Project file

---
+++ AssignmentTestApp/AssignmentTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentTestApp
+//
+//  Created by akash on 26/02/20.
+//  Copyright © 2020 akash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
